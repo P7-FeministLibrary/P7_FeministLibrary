e2a6277b985c09e4f5e0866f8ef33eb51cfdd028
AnaAguileraMorales88 <anitaaguilera@hotmail.com>
2025-10-02 09:16:12 +0200

    feat/author:add author model

--- src/main/java/com/feministlibrary/model/author/Author.java
+++ src/main/java/com/feministlibrary/model/author/Author.java
@@ -1,5 +1,58 @@
 package com.feministlibrary.model.author;
 
+import java.security.AuthProvider;
+
 public class Author {
-    
+    private int idAuthor;
+    private String name;
+    private String lastName;
+
+    public Author(){}
+
+    public Author(int idAuthor, String name, String lastName){
+        this.idAuthor = idAuthor;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Author(String name, String lastName){
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+
+    public int getIdAuthor() {
+        return this.idAuthor;
+    }
+
+    public void setIdAuthor(int idAuthor) {
+        this.idAuthor = idAuthor;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "idAuthor=" + idAuthor +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+
+    }
+
 }
