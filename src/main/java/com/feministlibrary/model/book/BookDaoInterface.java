3e2c82c16432dc1424718f779f19357e19a4c7f7
surayac <surayacsm@gmail.com>
2025-10-02 09:25:04 +0200

    feat(BookDAO): add bookDAO interface

---
+++ src/main/java/com/feministlibrary/model/book/BookDaoInterface.java
@@ -0,0 +1,20 @@
+package com.feministlibrary.model.book;
+
+import java.util.List;
+
+public class BookDaoInterface {
+
+    public interface BookDAOInterface {
+
+        void insert(Book book);
+        void update(Book book);
+        void delete(int idBook);
+        Book getById(int idBook);
+
+        List<Book> getAll();
+        List<Book> searchByTitle(String title);
+        List<Book> searchByAuthor(String authorName); 
+        List<Book> searchByGenre(String genreName);
+    }
+
+}
