0a6049259f38076d8d133068791d4996722731b2
surayac <surayacsm@gmail.com>
2025-10-03 14:31:01 +0200

    feat(AuthorDAO): add author DAO interface

---
+++ src/main/java/com/feministlibrary/model/author/AuthorDAOInterface.java
@@ -0,0 +1,16 @@
+package com.feministlibrary.model.author;
+
+import java.util.List;
+
+public interface AuthorDAOInterface {
+
+    void insert(Author author);
+    void update(Author author);
+    void delete(int idAuthor);
+    Author getById(int idAuthor);
+
+    List<Author> getAll();
+    List<Author> searchByName(String name);
+    List<Author> searchByLastName(String lastName);    
+}
+
