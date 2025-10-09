b6e5c4d8f8c1ead9c57331d2daf16cce006fe479
Abaraira <cibersteficodes@gmail.com>
2025-10-09 13:28:03 +0200

    deleted commented lines in DBManager.java

--- src/main/java/com/feministlibrary/config/DBManager.java
+++ src/main/java/com/feministlibrary/config/DBManager.java
@@ -29,31 +29,4 @@ public class DBManager {
     }
 
 
-    /*
-     * private static final String URL =
-     * "jdbc:postgresql://localhost:5432/feministlibrary";
-     * private static final String USER = "postgres";
-     * private static final String PASS = "6789";
-     * private static Connection connection;
-     * 
-     * public static Connection init() {
-     * try {
-     * connection = DriverManager.getConnection(URL, USER, PASS);
-     * System.out.println("Conexión existosa");
-     * } catch (Exception e) {
-     * System.out.println(e.getMessage());
-     * }
-     * return connection;
-     * }
-     * 
-     * public static void close() {
-     * try {
-     * connection.close();
-     * System.out.println("Desconexión exitosa");
-     * } catch (Exception e) {
-     * System.out.println(e.getMessage());
-     * }
-     * 
-     * }
-     */
 }
